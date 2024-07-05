9f5093182b582469056c944903161740074bcf9f
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-07-05 17:45:10 -0300

    start script download

--- script.sh
+++ script.sh
@@ -1,1 +1,13 @@
-sudo pacman -S xdg-desktop-portal pamixer pavucontrol pipewire xdg-desktop-portal xdg-desktop-portal-wlr swappy slurp
+# install ZSH, .oh-my.zsh powerlevel
+
+cp ./zsh/home/DOT-oh-my-zsh/* ~.oh-my-zsh
+cp ./zsh/home/DOT-zsh/* ~.zsh
+cp .zsh/home/powerlevel10k/* ~/powerlevel10k/
+
+#install alacritty
+cp ./alacritty/* ~/.config/alacritty/
+
+sudo pacman -S fzf xdg-desktop-portal pamixer pavucontrol pipewire xdg-desktop-portal xdg-desktop-portal-wlr swappy slurp
+
+# install hyperland
+cp ./hyperland/* ~/.config/hypr/
