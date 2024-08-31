6ed83409fa2b6e28d0f468aa734b3c04b51cd0da
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-08-31 12:45:37 -0300

    comment default layz vim setup

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+--    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
@@ -47,3 +47,5 @@ require("lazy").setup({
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+
