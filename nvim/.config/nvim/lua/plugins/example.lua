7364b200be299417fb26fc16cc2d9367832e5ea9
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-08-31 12:09:20 -0300

    add treesiter languages

--- nvim/.config/nvim/lua/plugins/example.lua
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -148,6 +148,11 @@ return {
       vim.list_extend(opts.ensure_installed, {
         "tsx",
         "typescript",
+        "javascript",
+        "java",
+        "go",
+        "gomod",
+        "json"
       })
     end,
   },
@@ -179,17 +184,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
 
   -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-      },
-    },
-  },
 
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
