aca2b739347dfbf5875918f76a87f58d3177742b
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-08-31 12:45:00 -0300

    snipts for lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -3,7 +3,10 @@ return {
     "williamboman/mason.nvim",
     -- lazy = false,
     config = function()
-      require("mason").setup()
+      require("mason").setup({
+        "typescript-language-server",
+        "eslint-lsp"
+      })
     end,
   },
   {
@@ -11,7 +14,15 @@ return {
     -- lazy = false,
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = {
+          "lua_ls",
+          "eslint",
+          "tsserver",
+          "stylua",
+          "shellcheck",
+          "shfmt",
+          "flake8",
+        }
       })
     end
   },
@@ -20,15 +31,33 @@ return {
     lazy = false,
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserve.setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+
+      lspconfig.lua_ls.setup({capabilities = capabilities})
+
+      lspconfig.tsserve.setup({
+        on_attach =  lspconfig.on_attach,
+        capabilities = capabilities,
+        init_options = {
+          preferences = {
+            disabpeSuggestions = false,
+          }
+        }
+      })
+
+      lspconfig.java_language_server.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
       vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', '<space>rf', vim.diagnostic.open_float)
     end,
   },
 }
