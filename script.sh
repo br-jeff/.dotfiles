8b7d137c0aa110c241cb0c90ea258793e6e93b97
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-09-01 11:45:01 -0300

    add script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,9 @@
 
 
 # ~;.local/share/fonts  Bitstream && Source Foundry
-sudo apt install stow flameshot
+sudo apt install stow flameshot fzf 
+sudo apt install openjdk-17-jdk
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
