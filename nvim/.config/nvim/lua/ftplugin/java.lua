ba23655b1606e7576321746bf913b6a6ca1ca65b
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-09-01 11:46:55 -0300

    java features

---
+++ nvim/.config/nvim/lua/ftplugin/java.lua
@@ -0,0 +1,32 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = vim.fn.expand("~/Documents/javaprojects/") .. project_name
+local java_path = vim.fn.systemlist('asdf which java')[1]
+
+local config = {
+  cmd = {
+    java_path,
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    '-jar',  vim.fn.expand("~/.local/share/nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar"),
+    '-configuration', vim.fn.expand("~/.local/share/nvim/mason/packages/jdtls/config_linux"),
+    '-data', workspace_dir
+  },
+  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  settings = {
+    java = {
+      completion = {
+        favoriteStaticMembers = { "org.junit.Assert.*", "org.junit.Assume.*", "org.junit.jupiter.api.Assertions.*", "org.junit.jupiter.api.Assumptions.*" }
+      },
+      -- Outras configurações específicas do JDTLS podem ser adicionadas aqui
+    },
+  },
+}
+
+require("jdtls").start_or_attach(config)
