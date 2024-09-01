9a7305ecadb139cfd5067a54a65f4098db90a6cc
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-09-01 11:46:43 -0300

    lsp config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,28 +1,27 @@
 return {
   {
     "williamboman/mason.nvim",
-    -- lazy = false,
     config = function()
       require("mason").setup({
-        "typescript-language-server",
-        "eslint-lsp"
+        -- Configurações adicionais para Mason
       })
-    end,
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    -- lazy = false,
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "eslint",
+          "gradle_ls",
           "tsserver",
-          "stylua",
-          "shellcheck",
-          "shfmt",
-          "flake8",
-        }
+          "html",
+          "cssls",
+          -- "jdtls",
+          "prismals",
+          "gopls",
+        },
+        automatic_installation = true,
       })
     end
   },
@@ -32,32 +31,54 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Configuração para clangd (C/C++)
       lspconfig.clangd.setup({
-        capabilities = capabilities
-      })
-
-      lspconfig.lua_ls.setup({capabilities = capabilities})
-
-      lspconfig.tsserve.setup({
-        on_attach =  lspconfig.on_attach,
         capabilities = capabilities,
-        init_options = {
-          preferences = {
-            disabpeSuggestions = false,
-          }
-        }
       })
 
-      lspconfig.java_language_server.setup({
-        capabilities = capabilities
+      -- Configuração para lua_ls (Lua)
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set('n', '<space>rf', vim.diagnostic.open_float)
+      -- Configuração para tsserver (TypeScript/JavaScript)
+      lspconfig.tsserver.setup({
+        on_attach = function(client, bufnr)
+          local bufopts = { noremap=true, silent=true, buffer=bufnr }
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+          vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
+          vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+          vim.keymap.set('n', '<space>rf', vim.diagnostic.open_float, bufopts)
+        end,
+        capabilities = capabilities,
+        settings = { documentFormatting = true },
+      })
+
+      lspconfig.gopls.setup({
+        on_attach = function(client, bufnr)
+          local bufopts = { noremap=true, silent=true, buffer=bufnr }
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+          vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
+          vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+          vim.keymap.set('n', '<space>rf', vim.diagnostic.open_float, bufopts)
+        end,
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
+      })
+
+      -- Adicione outras configurações de servidores de linguagem aqui
     end,
   },
 }
