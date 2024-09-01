eb2a476a1a23462c783dbca57183bdec5a9c318d
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-09-01 15:44:45 -0300

    script.sh asdf files

--- script.sh
+++ script.sh
@@ -3,7 +3,14 @@
 
 # ~;.local/share/fonts  Bitstream && Source Foundry
 sudo apt install stow flameshot fzf 
-sudo apt install openjdk-17-jdk
+
+
+# Below are default versions on .zshrc
+asdf install nodejs 14.21.3
+asdf install java adoptopenjdk-17.0.0+35
+asdf install golang 1.20.2
+
+
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 go install github.com/nametake/golangci-lint-langserver@latest
