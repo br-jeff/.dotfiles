bc15098d4e18ab8d403a6c378669f3c1fb2aef51
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-09-03 09:32:28 -0300

    set maven global

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ sudo apt install stow flameshot fzf
 asdf install nodejs 14.21.3
 asdf install java adoptopenjdk-17.0.0+35
 asdf install golang 1.20.2
-
+asdf global maven 3.5.4
 
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
