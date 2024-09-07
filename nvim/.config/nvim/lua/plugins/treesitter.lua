d6ba358558e2df1e7ed8e4686c7960de29e1f0bf
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-09-07 13:11:37 -0300

    rainbow brackets

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "HiPhish/nvim-ts-rainbow2" },
+  enabled = true,
   config = function()
     require("nvim-treesitter.configs").setup({
+      rainbow = {
+        enable = true,
+        query = "rainbow-parens",
+        strategy = require("ts-rainbow").strategy.global,
+      },
       ensure_installed = {
         "bash",
         "html",
@@ -21,23 +28,26 @@ return {
         "go",
         "gomod",
       },
+      sync_install = false,
       auto_install = true,
+      ignore_install = { "python" },
       indent = { enable = true },
       highlight = { enable = true },
       playground = { enable = false },
       textobjects = {
         select = {
-          enable = false,
+          enable = true,
           lookahead = true,
           keymaps = {
             ["af"] = "@function.outer",
             ["if"] = "@function.inner",
             ["ac"] = "@class.outer",
             ["ic"] = "@class.inner",
+            ["ib"] = "@block.inner",
           },
         },
       },
+      modules = {},
     })
-  end
+  end,
 }
-
