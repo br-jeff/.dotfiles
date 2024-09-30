181ae0b5fe34554073144da137efacf7571994ec
jefferson silva <jefferson_1996Th@hotmail.com>
2024-09-30 20:37:21 -0300

    add fullscreen shortcut

--- ubuntu-wm.sh
+++ ubuntu-wm.sh
@@ -152,6 +152,7 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Super><Sh
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Super><Shift>9']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
 
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
 
 
-
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
