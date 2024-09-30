b75ba1cb690909b3771931f6aeea5742e179c693
jefferson silva <jefferson_1996Th@hotmail.com>
2024-09-30 20:14:17 -0300

    add asdf.sh

---
+++ asdf-install.sh
@@ -0,0 +1,12 @@
+# Below are default versions on .zshrc
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin-add maven
+
+asdf install nodejs 14.21.3
+asdf install java adoptopenjdk-17.0.0+35
+asdf install golang 1.20.2
+asdf global maven 3.5.4
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latestth
