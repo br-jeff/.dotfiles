84da3de326bba2cb04f3533c60dc2016f7e09af9
jefferson silva <jefferson_1996Th@hotmail.com>
2024-09-30 20:13:40 -0300

    create essencial files

---
+++ apt-packages.sh
@@ -0,0 +1,6 @@
+sudo apt update
+code stow curl flameshot fzf dirmngr gpg awk apt-transport-https neovim snap --yes
+sudo snap install dbeaver-ce
+sudo apt-get install libproxy1-plugin-networkmanager gnome-shell-extension-system-monitor --yes
+sudo apt install gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 gnome-system-monitor  gettext  --yes
+sudo apt install gnome-shell-extension-manager --yes
