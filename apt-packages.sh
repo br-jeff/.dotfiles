7782a45bb9ee1d35538fc04e887365f0dbf3e426
Jefferson Ferreira da silva <jefferson_1996Th@hotmail.com>
2025-04-19 20:08:30 -0300

    waybar update

--- apt-packages.sh
+++ apt-packages.sh
@@ -4,3 +4,5 @@ sudo snap install dbeaver-ce
 sudo apt-get install libproxy1-plugin-networkmanager gnome-shell-extension-system-monitor --yes
 sudo apt install gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 gnome-system-monitor  gettext  --yes
 sudo apt install gnome-shell-extension-manager --yes
+
+sudo apt install sway-notification-center
